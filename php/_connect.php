14a56bf76ac1918efbcdf62bcce2ee74e5a47ac7
sjwatts119 <samuel.watts67@gmail.com>
2024-01-15 15:11:04 +0000

    Connect File now uses Environment Variables
    .env file must be in root folder for the connect file to be populated correctly.

--- php/_connect.php
+++ php/_connect.php
@@ -1,9 +1,14 @@
 <?php
 
-$db_server = "plesk.remote.ac";
-$db_user = "WS314664_teachitAdmin";
-$db_password = "Jzp27o#90";
-$db_database = "WS314664_teachitTesting";
+require_once($_SERVER['DOCUMENT_ROOT'].'/php/readEnvVars.php');
+
+GetSettings();
+
+$db_server = $_ENV['DB_HOST'];
+$db_user = $_ENV['DB_USER'];
+$db_password = $_ENV['DB_PASS'];
+$db_database = $_ENV['DB_NAME'];
+
 
 //connect to database server
 $db_connect = mysqli_connect($db_server,$db_user,$db_password,$db_database)
