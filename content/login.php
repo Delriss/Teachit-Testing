7dbb1375556cf180c18309b0387ff1e6c6671560
sjwatts119 <samuel.watts67@gmail.com>
2024-01-15 13:25:15 +0000

    Connection File Made
    Test Login Page now runs database Connection

--- content/login.php
+++ content/login.php
@@ -1,1 +1,3 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/php/_connect.php'); ?>
+
 Login Page
---
+++ php/_connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$db_server = "plesk.remote.ac";
+$db_user = "WS314664_teachitAdmin";
+$db_password = "Jzp27o#90";
+$db_database = "WS314664_teachitTesting";
+
+//connect to database server
+$db_connect = mysqli_connect($db_server,$db_user,$db_password,$db_database)
+
+?>
