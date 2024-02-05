009328bdd77f28281230cbbfb7a6f44a97fd3480
WS303555 <WS303555@weston.ac.uk>
2024-02-05 13:37:32 +0000

    Changed the title of the page.
    The title was still listed as "Template - TeachIT Testing". I have now changed to a permanent name instead of the placeholder from _template.php

--- content/index.php
+++ content/index.php
@@ -10,7 +10,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" author="SEF">
-    <title>Template - TeachIt Testing</title>
+    <title>TeachIt Testing</title>
 
     <!-- Stylesheets -->
     <link rel="stylesheet" href="css/style.css">
