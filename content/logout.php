326aefed808ae6978d9723896a1b44ab54ef7ff8
Finley Pettit <67767347+Delriss@users.noreply.github.com>
2024-02-27 15:34:36 +0000

    Logout page fix

--- content/logout.php
+++ content/logout.php
@@ -1,10 +1,5 @@
 <?
-
-if(!isset($_SESSION)) {
-    die('<p class="lead">User is not logged in.</p>');
-}
-else{
-    session_destroy();
-    header("Location: /");
-    die();
-}
+session_start();
+session_destroy();
+header("Location: /");
+die();
