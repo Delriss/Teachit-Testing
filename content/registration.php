06bdccb7b41814fb87aeb42a0c70959f3975d2aa
Finley Pettit <67767347+Delriss@users.noreply.github.com>
2024-02-27 11:01:52 +0000

    Update registration and login links and inclusion of htaccess restrictions on accessing folders

--- content/registration.php
+++ content/registration.php
@@ -89,7 +89,7 @@
             </div>
             <button type="submit" class="btn btn-dark rounded-pill">Register</button>
             <hr>
-            <p>Already have an account? <a id="loginText" href="login.php">Login Here!</a></p>
+            <p>Already have an account? <a id="loginText" href="/login">Login Here!</a></p>
         </form>
     </div>
     <!-- End Main Content -->
