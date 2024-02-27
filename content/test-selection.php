63645830b61a933ff1b310ae19c2eb049cdb4653
Finley Pettit <67767347+Delriss@users.noreply.github.com>
2024-02-27 11:37:38 +0000

    Updating of Test-Selection as branch pull was incorrect

--- content/test-selection.php
+++ content/test-selection.php
@@ -1,100 +1,128 @@
 <?php
-//Remove direct access to the file and require login
-if ($_SESSION['LoggedIn'] == false) {
-    header("Location: /login");
-    die();
-}
+// DEBUGGING - Start Session and set UID to 1
+$_SESSION['UID'] = 1;
 ?>
 
+
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-100" lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="SEF">
-    
+
     <title>Test Selection - TeachIt Testing</title>
 
     <!-- Stylesheets -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../content/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <!-- End Stylesheets -->
-
 </head>
 
-<body>
-    <header>
-        <!-- Navigation -->
-        <nav class="navbar navbar-expand-lg primaryBG">
-            <div class="container-fluid text-white">
-                <a href="index.php"><img src="imgs/logo-side.png" alt="TeachIt Logo" class="logo ps-5" width="350"></a>
-
-                <!-- Page Navigation -->
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <ul class="nav justify-content-end">
-                                <li class="nav-item">
-                                    <a class="nav-link text-white fs-4 subtitle active" href="index.php">Home</a>
-                                </li>
-
-                                <!-- Check if user is logged in -->
-                                <?php if (isset($_SESSION)) : ?>
-                                    <li class="nav-item">
-                                        <a class="nav-link text-white subtitle fs-4" href="logout.php">Test Management</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link text-white subtitle fs-4" href="logout.php">Logout</a>
-                                    </li>
-                                <?php endif; ?>
-
-                                <?php if (!isset($_SESSION)) : ?>
-                                    <li class="nav-item">
-                                        <a class="nav-link text-white subtitle fs-4" href="login.php">Login</a>
-                                    </li>
-                                <?php endif; ?>
-
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Page Navigation -->
-
+<body class="d-flex flex-column h-100">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bgColour">
+        <!-- Nav Container - adapt to screen size -->
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <img src="../content/imgs/logo-side.png" alt="TeachIt Testing" width="200" height="50">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapsable" aria-controls="navbarCollapsable" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapsable">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/test-selection">Test Selection</a>
+                    </li>
+                    <li class="nav-item dropdown bgColour">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Admin Dashboard
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Student Management</a></li>
+                            <li><a class="dropdown-item" href="#">Test Management</a></li>
+                            <li><a class="dropdown-item" href="#">Lecturer Management</a></li>
+                            <li><a class="dropdown-item" href="#">Statistics</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-        <!-- End Navigation -->
-    </header>
+        </div>
+        <!-- End Nav Container -->
+    </nav>
+    <!-- End Navbar -->
 
     <!-- Main Content -->
-    <div class="area">
-        <main class="d-flex justify-content-center">
-            <div class="container d-flex flex-column align-items-center pt-5">
-                <h1>Main Content Area / Title</h1>
-                <p>Content Goes Here</p>
+    <div class="container-fluid text-center">
+        <!-- New Test Selection Area -->
+        <div class="row">
+            <div class="col-12">
+                <h1 class="display-4 mt-5">Test Selection</h1>
+                <p class="lead">Please select a test to take</p>
             </div>
-        </main>
-        <!-- End Main Content -->
+        </div>
 
-        <footer class="container-fluid primaryBG w-100">
-            <div class="footerContainer container-fluid d-flex justify-content-center align-items-center h-100 text-center subtitle text-white">
-                <div class="container-fluid d-flex justify-content-evenly">
-                    <span>TeachIt Testing &copy; 2021</span>
-                    <a id="privPolicy" href="./">Privacy Policy</a>
+        <hr class="my-4">
+
+        <!-- Test Cards -->
+        <div id="testContainer" class="row">
+            <!-- BASIC TEST CARD TEMPLATE -->
+            <!-- <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Test 1</h5>
+                        <p class="card-text">Test Description</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="test.php" class="btn btn-primary">Start Test</a>
+                    </div>
                 </div>
-                <div class="container-fluid d-flex justify-content-evenly">
-                    <a href="https://www.facebook.com/"><i class="fab fa-facebook-square fa-2x"></i></a>
-                    <a href="https://www.instagram.com/"><i class="fab fa-instagram-square fa-2x"></i></a>
-                    <a href="https://www.twitter.com/"><i class="fab fa-twitter-square fa-2x"></i></a>
-                </div>
+            </div> -->
+            <!-- END BASIC TEST CARD TEMPLATE -->
+
+            <!-- PHP GENERATED TEST CARDS WILL BE POPULATED HERE. 
+            AJAX request queries retrieveTests.php to retrieve custom test cards -->
+
+        </div>
+
+        <!-- Completed Test Cards -->
+        <div class="row">
+            <div class="col-12">
+                <h1 class="display-4 mt-5">Completed Tests</h1>
+                <p class="lead">View your completed tests</p>
             </div>
-        </footer>
+        </div>
+
+        <hr class="my-4">
+
+        <div id="completedTestContainer" class="row mb-5">
+
+            <!--PHP GENERATED COMPLETED TEST CARDS WILL BE POPULATED HERE.
+        AJAX request queries retrieveCompletedTests.php to retrieve custom test cards -->
+
+        </div>
     </div>
+    <!-- End Main Content -->
 
+    <!-- Footer -->
+    <footer class="footer mt-auto py-3 rounded-top bgColour">
+        <div class="container text-center">
+            <span class="text-muted">TeachIt Testing &copy; 2021</span>
+        </div>
+    </footer>
+    <!-- End Footer -->
 
     <!-- Background Circles -->
     <ul class="circles">
@@ -110,10 +138,14 @@ if ($_SESSION['LoggedIn'] == false) {
         <li></li>
     </ul>
     <!-- End Background Circles -->
+
+
+
 </body>
 <!-- JS -->
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="js/scripts.js"></script>
+<script src="../content/js/scripts.js"></script>
+
 </html>
