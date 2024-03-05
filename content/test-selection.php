dc0aac484a4a02de3dc8c3263be94cf7dfa844ae
Finley Pettit <67767347+Delriss@users.noreply.github.com>
2024-03-05 12:21:19 +0000

    Addition of exisiting infrastructure to test-selection to add scheduling

--- content/test-selection.php
+++ content/test-selection.php
@@ -72,7 +72,97 @@ if ($_SESSION['LoggedIn'] == false) {
         <div class="row">
             <div class="col-12">
                 <h1 class="display-4 mt-5">Test Selection</h1>
-                <p class="lead">Please select a test to take</p>
+                <p class="lead">Please select a test to take or create a test</p>
+                <button type="button" class="btn btn-primary mx-3" data-bs-toggle="modal" data-bs-target="#createTestModal">Create Test</button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="createTestModal" tabindex="-1" aria-labelledby="testNameLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="testNameLabel">New Test</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form id="createTestForm">
+                                    <div class="mb-3">
+                                        <label for="testName" class="form-label">Test Name</label>
+                                        <input type="text" class="form-control" id="testName" name="testName" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="testDescription" class="form-label">Test Description</label>
+                                        <textarea class="form-control" id="testDescription" name="testDescription" required></textarea>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="testSubject" class="form-label">Subject</label>
+                                        <select class="form-select" id="testSubject" name="testSubject" required>
+                                            <option value="" selected disabled>Select a Subject</option>
+                                        </select>
+                                    </div>
+                                    <!-- add a date/time section which can be enabled and disabled through a tickbox, the tickbox is inline with the date/time input -->
+                                    <div class="mb-3">
+                                        <label for="testDateTime" class="form-label">Test Date & Time</label>
+                                        <input type="datetime-local" class="form-control" id="testDateTime" name="testDateTime" required disabled>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" id="enableDateTime" name="enableDateTime">
+                                            <label class="form-check-label" for="enableDateTime">Enable Date & Time</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion accordion-flush" id="accordionFlush">
+                                        <div class="accordion-item" id="questionAccordionItem">
+                                            <h2 class="accordion-header" id="flush-heading1">
+                                                <div class="d-flex align-items-center questionAccordionButtonContainer">
+                                                    <button class="accordion-button collapsed flex-grow-1" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse1" aria-expanded="false" aria-controls="flush-collapse1">
+                                                        Question 1
+                                                    </button>
+                                                </div>
+                                            </h2>
+                                            <div id="flush-collapse1" class="accordion-collapse collapse" aria-labelledby="flush-heading1" data-bs-parent="#accordionFlush">
+                                                <div class="accordion-body">
+                                                    <div class="mb-3">
+                                                        <label class="form-label">Question</label>
+                                                        <input type="text" class="form-control questions" data-question="1" name="question" required>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label class="form-label">Answer 1</label>
+                                                        <input type="text" class="form-control answers" data-question="1" name="answer" required>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label class="form-label">Answer 2</label>
+                                                        <input type="text" class="form-control answers" data-question="1" name="answer" required>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label class="form-label">Answer 3</label>
+                                                        <input type="text" class="form-control answers" data-question="1" name="answer" required>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label class="form-label">Answer 4</label>
+                                                        <input type="text" class="form-control answers" data-question="1" name="answer" required>
+                                                    </div>
+                                                    <input class="form-check-input answerRadio1" type="radio" name="isCorrect1" data-question="1" checked>
+                                                    <label class="form-check-label" for="isCorrect1">Answer 1</label>
+                                                    <input class="form-check-input answerRadio2" type="radio" name="isCorrect1" data-question="1">
+                                                    <label class="form-check-label" for="isCorrect1">Answer 2</label>
+                                                    <input class="form-check-input answerRadio3" type="radio" name="isCorrect1" data-question="1">
+                                                    <label class="form-check-label" for="isCorrect1">Answer 3</label>
+                                                    <input class="form-check-input answerRadio4" type="radio" name="isCorrect1" data-question="1">
+                                                    <label class="form-check-label" for="isCorrect1">Answer 4</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- allow the user to add more questions -->
+                                    <button type="button" class="btn btn-primary" id="addQuestion">Add Question</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary" id="submitForm">Add Test</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -149,5 +239,7 @@ if ($_SESSION['LoggedIn'] == false) {
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="../content/js/scripts.js"></script>
+<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+<script src="../content/js/test-management.js"></script>
 
 </html>
--- php/outputStudentTests.php
+++ php/outputStudentTests.php
@@ -8,21 +8,39 @@ if (!isset($_SESSION)) {
 include_once($_SERVER['DOCUMENT_ROOT'] . '/php/_connect.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/php/retrieveTests.php');
 
+////Change Request////
+//Output the tests
 foreach ($testArray as $test) {
-    echo ('<div class="col-sm-4 mb-4">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">' . $test->title . '</h5>
-                    <p class="card-text">' . $test->subject . ' - ' . count($test->questions) . ' Questions</p>
-                    <hr>
-                    <p class="card-text">' . $test->testDesc . '</p>
-                </div>
-                <div class="card-footer">
-                    <a href="test.php" class="btn btn-primary">Start Test</a>
-                </div>
-            </div>
-        </div>
-    ');
-};
 
-?>
+    $upcoming = false;
+    if ($test->testDateTime != null) {
+        //check to see if the test date and time is in the future
+        $testDateTime = new DateTime($test->testDateTime);
+
+        //get the current date and time
+        $currentDateTime = new DateTime();
+
+        //Set the upcoming variable to true if the test date and time is in the future
+        if ($testDateTime > $currentDateTime) {
+            $upcoming = true;
+        }
+    }
+
+    //If upcoming do not display the test else display the test
+    if (!$upcoming) {
+        echo ('<div class="col-sm-4 mb-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">' . $test->title . '</h5>
+                        <p class="card-text">' . $test->subject . ' - ' . count($test->questions) . ' Questions</p>
+                        <hr>
+                        <p class="card-text">' . $test->testDesc . '</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="test.php" class="btn btn-primary">Start Test</a>
+                    </div>
+                    </div>
+                </div>
+    ');
+    }
+};
