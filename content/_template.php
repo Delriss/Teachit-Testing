41b186aee99e93c1a7e398a7d9e753ebc163ff02
Finley Pettit <67767347+Delriss@users.noreply.github.com>
2024-03-11 11:48:02 +0000

    Fixing of template page for partial implementations

--- content/_template.php
+++ content/_template.php
@@ -14,34 +14,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/content/partials/header.php');
     </div>
     <!-- End Main Content -->
 
-    <!-- Footer -->
-    <footer class="footer mt-auto py-3 rounded-top bgColour">
-        <div class="container text-center">
-            <span class="text-muted">TeachIt Testing &copy; 2021</span>
-        </div>
-    </footer>
-    <!-- End Footer -->
-
-    <!-- Background Circles -->
-    <ul class="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-    <!-- End Background Circles -->
-
-</body>
-<!-- JS -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="../content/js/scripts.js"></script>
-
-</html>
+    <?php
+    //include the footer
+    include($_SERVER['DOCUMENT_ROOT'] . '/content/partials/footer.php');
+    ?>
