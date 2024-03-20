fc7f0a4d705c21923f54af761902b1ada91ec654
sjwatts119 <samuel.watts67@gmail.com>
2024-03-20 14:09:16 +0000

    Bootstrap Toogleable Tabs ready for all Subjects.

--- content/leaderboard.php
+++ content/leaderboard.php
@@ -6,7 +6,31 @@ include($_SERVER['DOCUMENT_ROOT'] . '/content/partials/header.php');
 
 <body class="d-flex flex-column h-100">
 
-THIS IS CONTENT
+    <div class="w-75 h-75 mx-auto mt-5 border" id="leaderboardContainer">
+
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+        </div>
+
+    </div>
+
+    <?php 
+    //include retrieveLeaderboardData.php to test the leaderboard
+    include($_SERVER['DOCUMENT_ROOT'] . '/php/retrieveLeaderboardData.php');
+    ?>
 
 <?php
 //include the footer
