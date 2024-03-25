b3995a39275f3ed5f04c6cdc8f16eb007c755fbb
sjwatts119 <samuel.watts67@gmail.com>
2024-03-25 15:27:02 +0000

    Removal of testManagement.css which is no longer in use.
    This file is no longer in use as per the last commit on this branch which moved the contents into style.css. This can now be removed safely.

--- content/partials/header.php
+++ content/partials/header.php
@@ -22,7 +22,6 @@ if (isset($_SESSION['user'])) {
     <!-- Stylesheets -->
     <!-- linking css using routing wrapper -->
     <link rel="stylesheet" href="/public_static/css/style.css">
-    <link rel="stylesheet" href="/public_static/css/testManagement.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
